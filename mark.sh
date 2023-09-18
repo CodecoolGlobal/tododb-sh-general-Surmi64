76847b2fc5becfd259d64c4ea1743f1b470e5190
Surmi64 <surmi64@gmail.com>
2023-09-18 16:04:44 +0200

    mark.sh implemented, every function is working properly

--- mark.sh
+++ mark.sh
@@ -8,8 +8,32 @@
 #    mark.sh unmark-todo 32
 #
 
+mark_todo() {
+    psql <<EOF
+    UPDATE "todos"
+    SET done = true
+    WHERE todo_id = $1;
+EOF
+    echo "Todo marked as done!"
+}
+
+unmark_todo() {
+    psql <<EOF
+    UPDATE "todos"
+    SET done = false
+    WHERE todo_id = $1;
+EOF
+    echo "Todo marked as not done yet!"
+}
+
 main() {
-    echo "Your code"
+    if [[ "$1" == "mark-todo" ]]
+    then
+        mark_todo "$2"
+    elif [[ "$1" == "unmark-todo" ]]
+    then
+        unmark_todo "$2"
+    fi
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
