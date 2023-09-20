1a7bd9611598e53a62fd841021321ec2eb3cd5bd
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2023-09-20 15:32:54 +0200

    mark todos as true or false

--- mark.sh
+++ mark.sh
@@ -8,8 +8,29 @@
 #    mark.sh unmark-todo 32
 #
 
+mark_todo() {
+    psql <<EOF
+UPDATE "todos" SET done = true WHERE todo_id=('$1')
+EOF
+}
+
+unmark_todo() {
+    psql <<EOF
+UPDATE "todos" SET done = false WHERE todo_id = '$1'
+EOF
+}
+
+
 main() {
-    echo "Your code"
+    if [[ "$1" == "mark-todo" ]]
+    then 
+        echo "1"
+        mark_todo "$2"
+    elif [[ "$1" == "unmark-todo" ]]
+    then
+        echo "2"
+        unmark_todo "$2"
+    fi
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
