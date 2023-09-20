1846147c627c7804fcc059e3c6ad9881a0d1c142
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2023-09-20 15:33:15 +0200

    Delete todos by id, and all true done

--- delete.sh
+++ delete.sh
@@ -8,4 +8,29 @@
 #    delete.sh delete-done
 #
 
-echo "Your code"
+delete_todo() {
+    psql <<EOF
+DELETE FROM todos WHERE todo_id = '$1'
+EOF
+}
+
+delete_done() {
+    psql <<EOF
+DELETE FROM todos WHERE done = true
+EOF
+}
+
+main() {
+    if [[ "$1" == "delete-todo" ]]
+    then
+        delete_todo "$2"
+    elif [[ "$1" == "delete-done" ]]
+    then
+        delete_done
+    fi
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+then
+    main "$@"
+fi
