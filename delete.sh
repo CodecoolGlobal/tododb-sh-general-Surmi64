6c6ff0eb7c7393f1a2132892c1ae18e27f457808
norbert <kulcsar.norbert.86@gmail.com>
2023-09-22 09:30:23 +0200

    Delete Script -> Done

--- delete.sh
+++ delete.sh
@@ -9,3 +9,29 @@
 #
 
 echo "Your code"
+
+delete_todo(){
+psql <<EOF
+  DELETE FROM basic_schema.todo
+  WHERE basic_schema.todo.id = '$1'
+EOF
+}
+
+delete_done(){
+psql <<EOF
+  DELETE FROM basic_schema.todo
+  WHERE basic_schema.todo.done = true
+EOF
+}
+
+main(){
+  if [[ "$1" == "delete-todo" ]]
+  then delete_todo "$2"
+  elif [[ "$1" == "delete-done" ]]
+  then delete_done
+  fi
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+then main "$@"
+fi
