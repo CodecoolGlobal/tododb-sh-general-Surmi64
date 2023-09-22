fa25e0fb3e49a1dcd970ca820dc79075b90cba14
norbert <kulcsar.norbert.86@gmail.com>
2023-09-22 09:29:52 +0200

    Mark Script -> Done

--- mark.sh
+++ mark.sh
@@ -8,8 +8,31 @@
 #    mark.sh unmark-todo 32
 #
 
+mark_todo(){
+    psql <<EOF
+    UPDATE basic_schema.todo
+    SET "done" = true
+    WHERE basic_schema.todo.id =('$1')
+EOF
+echo "Marked as done."
+}
+
+unmark_todo(){
+    psql <<EOF
+    UPDATE basic_schema.todo
+    SET "done" = false
+    WHERE basic_schema.todo.id =('$1')
+EOF
+}
+
 main() {
-    echo "Your code"
+if [[ "$1" == "mark-todo" ]]
+then
+  mark_todo "$2"
+elif [[ "$1" == "unmark-todo" ]]
+then
+  unmark_todo "$2"
+fi
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
